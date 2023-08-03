80b5ef202763cd55ca898dabe46a9b90f8209140
shaha <ayubjonovshakhzod01@gmail.com>
2023-08-03 13:55:30 +0500

    added some condition

--- javascript/index.js
+++ javascript/index.js
@@ -8,15 +8,19 @@ async function getUtmInviteLink() {
 
 const setUrls = (channelInviteUrl, botStartUrl) => {
   // channel-invite-link
-  const channelInviteLink = document.getElementsByClassName('channel-invite-link');
-  for (let i = 0; i < channelInviteLink.length; i++) {
-    channelInviteLink[i].setAttribute('href', channelInviteUrl);
+  if(channelInviteUrl) {
+    const channelInviteLink = document.getElementsByClassName('channel-invite-link');
+    for (let i = 0; i < channelInviteLink.length; i++) {
+      channelInviteLink[i].setAttribute('href', channelInviteUrl);
+    }
   }
 
   // bot-start-link
-  const botStartLink = document.getElementsByClassName('bot-start-link');
-  for (let i = 0; i < botStartLink.length; i++) {
-    botStartLink[i].setAttribute('href', botStartUrl);
+  if(botStartUrl) {
+    const botStartLink = document.getElementsByClassName('bot-start-link');
+    for (let i = 0; i < botStartLink.length; i++) {
+      botStartLink[i].setAttribute('href', botStartUrl);
+    }
   }
 }
 
